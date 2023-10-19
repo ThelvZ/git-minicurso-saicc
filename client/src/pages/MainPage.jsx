41c28a9d1aa8f9810cb3037b3f1f615b121fafc6
Théo Corvello Soares <134558560+ThelvZ@users.noreply.github.com>
2023-10-19 11:57:37 -0300

    feat: new profile Théo

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,72 +1,96 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from 'react-router-dom';
 
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
+import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-export default function MainPage(){
-    return (
-        <div className="bg-white">
-          <div className="relative isolate px-6 pt-14 lg:px-8">
-            <div
-              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-              aria-hidden="true"
-            >
-              <div
-                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#80ffff] to-[#837dcc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                style={{
-                  clipPath:
-                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                }}
-              />
-            </div>
-            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                  Para mais informações, leia a documentação:{' '}
-                  <a 
-                    href="https://github.com/romulodm/git-minicurso-saicc#readme" 
-                    className="font-semibold text-indigo-600"
-                    target="_blank"
-                  >
-                    <span className="absolute inset-0" aria-hidden="true" />
-                        Saber mais<span aria-hidden="true">&rarr;</span>
-                  </a>
+
+export default function TheoProfile() {
+
+  return (
+    <div className="flex flex-col min-h-screen border-none screen rounded-lg bg-white px-4 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4 ">
+
+        <div className="flex flex-col w-full h-full py-16">
+            <div className="container mx-auto px-4">
+              <div className="relative h-20 rounded-b flex justify-center">
+                <div className="absolute -bottom-6">
+                  {/* Sua foto aqui: */}
+                    <img
+                      src="https://i.imgur.com/EDoL61t.jpg"
+                      className="object-cover border-4 border-white w-40 h-40 rounded-full"
+                      alt="cover"
+                    />
                 </div>
               </div>
-              <div className="text-center">
-                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                  Minicurso de GIT
-                </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Esse projeto foi feito com o intuito de exemplificar o uso colaborativo do GitHub.
-                  Aqui dentro temos alguns perfis de participantes da XIII SAICC.
-                </p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                  <a
-                    href="https://github.com/romulodm/git-minicurso-saicc"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    target="_blank"
-                  >
-                    Repositório no GitHub
-                  </a>
-                  <NavLink to="/example">
-                    <p className="text-sm font-semibold leading-6 text-gray-900">Exemplo de perfil <span aria-hidden="true">→</span></p>
-                  </NavLink>
+              <div className="text-center mt-12">
+                {/* Seu nome aqui: */}
+                <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
+                  Théo Soares
+                </h3>
+
+                <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                  <MapOutlinedIcon style={{color: '#FDAE17'}}/>                                           
+                  {/* Sua cidade aqui: */}
+                  <span className="align-items-center" style={{marginLeft: '5px'}}>
+                    São Lourenço do Sul, RS
+                  </span>
+                </div>
+
+                <div className="mb-2 text-blueGray-600 mt-10">
+                  <AccountBalanceOutlinedIcon style={{color: '#FDAE17'}}/>                       
+                  {/* A universidade aqui: */}
+                  <span className="align-items-center" style={{marginLeft: '5px'}}>
+                     Universidade Federal do Rio Grande
+                  </span>
+                </div>
+
+                <div className="mb-2 text-blueGray-600 mt-5">
+                  <SchoolOutlinedIcon style={{color: '#FDAE17'}}/>                        
+                  {/* O curso aqui: */}
+                  <span className="align-items-center" style={{marginLeft: '5px'}}>
+                    Engenharia de Computação
+                  </span>
+                </div>
+              </div>
+
+              <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
+                <div className="flex flex-wrap justify-center">
+                  <div className="w-full lg:w-9/12 px-4">                           
+                    {/* Biografia aqui: */}
+                    <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
+                    Meu nome é Théo Corvello Soares, tenho 19 anos e nasci no dia 11/09/2004 na cidade de São Lourenço do Sul e gosto de jogar cs e xingar boludos (no cs obviamente)
+                    </p>
+                    <br/>
+                    {/* Link para seu GitHub aqui: */}
+                    <a href="https://github.com/Thelvz/git-minicurso-saicc" className="font-normal text-yellow-500">
+                      <GitHubIcon/>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
-            <div
-              className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-              aria-hidden="true"
-            >
-              <div
-                className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#fff89b] to-[#e79121] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                style={{
-                  clipPath:
-                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                }}
-              />
+        </div>
+      </div>
+
+      <footer className="relative bg-blueGray-200 pt-8 pb-6 mt-8">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap items-center md:justify-between justify-center">
+            <div className="w-full md:w-6/12 px-4 mx-auto text-center">
+              <NavLink to="/">
+                <div className="text-sm text-blueGray-500 font-semibold py-1">
+                  <p>Home</p>
+                </div>
+              </NavLink>
+              <div className="text-sm text-blueGray-500 mt-3 font-semibold py-1">
+                <a href="https://www.saicc.com.br/" className="text-blueGray-500 hover:text-blueGray-800" target="_blank">SAICC - 2023</a>
+              </div>
             </div>
           </div>
         </div>
-      )
-}
+      </footer>
+    </div>
+  );
+};
